1fdba671c359f9e8c28bb1949fc5bc5e4a6001d0
Minjae <minjae.jung0@gmail.com>
2019-12-30 01:13:48 -0500

    Update Login

--- frontend/tenmen-app/src/components/HelloWorld.vue
+++ frontend/tenmen-app/src/components/HelloWorld.vue
@@ -1,32 +1,15 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <label for="name">Username (4 to 8 characters):</label>
+
+<input type="text" id="username" name="name" required
+       minlength="4" maxlength="8" size="10" v-model="user.username">
+<label for="name">Password (4 to 8 characters):</label>
+<input type="text" id="password" name="name" size="10" v-model="user.password" >
+<button class="favorite styled" type="button" size="10" @click="loginClick">
+    Login
+</button>
+
   </div>
 </template>
 
@@ -35,7 +18,20 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  methods: {
+    loginClick() {
+      alert(this.user.username + this.user.password) 
+    }
+  },
+  data() {
+    return {
+      user:{
+        username: "",
+        password: ""
+      }
+      }
+  },
 }
 </script>
 
