fd58906b15826683a1398c6fb15f6b5a18d0dce1
Minjae <minjae.jung0@gmail.com>
2020-01-02 20:14:58 -0500

    update

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 app = Flask(__name__)
 
--- frontend/tenmen-app/src/components/HelloWorld.vue
+++ frontend/tenmen-app/src/components/HelloWorld.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="hello">
-   <label for="name">Username (4 to 8 characters):</label>
-
-<input type="text" id="username" name="name" required
+    <label for="name">Username (4 to 8 characters):</label>
+    <input type="text" id="username" name="name" required
        minlength="4" maxlength="8" size="10" v-model="user.username">
-<label for="name">Password (4 to 8 characters):</label>
-<input type="text" id="password" name="name" size="10" v-model="user.password" >
-<button class="favorite styled" type="button" size="10" @click="loginClick">
-    Login
-</button>
-
+    <label for="name">Password (4 to 8 characters):</label>
+    <input type="text" id="password" name="name" size="10" v-model="user.password" >
+    <button class="favorite styled" type="button" size="10" @click="loginClick">
+      Login
+    </button>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
@@ -34,7 +31,6 @@ export default {
   },
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
