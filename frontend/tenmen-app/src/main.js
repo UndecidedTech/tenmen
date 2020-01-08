db7199dc092907c2aa9122f19f19b555943dad50
Brendan Powers <bpowers.soccer@gmail.com>
2020-01-08 01:17:47 -0500

    updates

--- frontend/tenmen-app/src/main.js
+++ frontend/tenmen-app/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Signup from "./Signup.vue"
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: "/", component: App },
+  {path: "/signup", component:  }
+]
 
 new Vue({
   render: h => h(App),
