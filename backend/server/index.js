7007850920100497064194498ae1c2a5bccafdd5
Brendan Powers <brendanpowers@Brendans-MBP.domain.local>
2020-02-05 19:40:28 -0500

    this one goes out to Jacob Colvin

---
+++ backend/server/index.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line strict
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+app.use(cookieParser());
+const session = require("express-session");
+
+
+app.use(session({
+    secret: "secret",
+    resave: false,
+    httpOnly: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 60000
+    }
+}));
+
+app.use(bodyParser.json());
+app.use(cors({
+    "origin": ["http://localhost:8080"],
+    "credentials": true,
+    "methods": ["GET", "POST"]
+}));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on ${port}`));
